49e6b7314d0500483aa1d40dc37666d08ddf82f7
Mahmoud_Walid <modywmbadr@gmail.com>
2025-11-23 23:30:01 +0200

    feat: add AppSidebar component for admin navigation

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,58 @@
+import { Settings, Users } from 'lucide-react';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarTrigger,
+} from './ui/sidebar';
+
+const items = [
+  {
+    title: 'Users',
+    url: '/admin/users',
+    icon: Users,
+  },
+  {
+    title: 'Settings',
+    url: '/admin/settings',
+    icon: Settings,
+  },
+];
+
+const AppSidebar = () => {
+  return (
+    <Sidebar className="!mt-auto !h-[calc(100%-4rem)]">
+      <SidebarHeader />
+      <SidebarTrigger className="absolute top-2 -right-7 rounded-md rounded-l-none border border-l-0 border-zinc-500 bg-zinc-500/40" />
+      <SidebarContent className="!px-4">
+        <SidebarGroup />
+        <SidebarGroupLabel>Admin</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
