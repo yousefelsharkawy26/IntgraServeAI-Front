38badb3b5f92842e8ba95b4626e96c5479fa2172
Mahmoud_Walid <modywmbadr@gmail.com>
2025-11-23 23:51:19 +0200

    feat(admin): add user creation modal and form components
    Implement modal dialog and form for creating new users with email, password, full name and role selection. Includes form validation, loading states, and success/error handling.

---
+++ src/features/admin/FormCreateUser.tsx
@@ -0,0 +1,161 @@
+import InputPassword from '@/components/InputPassword';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import {
+  createUserRequestSchema,
+  createUserRequestT,
+} from '@/schema/admin/createUserSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FieldErrors, SubmitHandler, useForm } from 'react-hook-form';
+import { useAllRoles } from './useAllRoles';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Spinner } from '@/components/ui/spinner';
+
+interface IProps {
+  onSubmit: SubmitHandler<createUserRequestT>;
+  onError: SubmitHandler<FieldErrors>;
+  classNames?: {
+    inputClassName?: string;
+  };
+}
+
+const FormCreateUser = ({ onSubmit, onError, classNames = {} }: IProps) => {
+  const form = useForm<createUserRequestT>({
+    resolver: zodResolver(createUserRequestSchema),
+    mode: 'onTouched',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      email: '',
+      password: '',
+      full_name: '',
+      roles_id: [],
+    },
+  });
+
+  const { dataAllRoles, isLoadingAllRoles } = useAllRoles();
+
+  return (
+    <Form {...form}>
+      <form
+        id="create-user-form"
+        onSubmit={form.handleSubmit(onSubmit, onError)}
+        className="flex flex-col gap-4 space-y-6"
+      >
+        {/* email field */}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  className={classNames.inputClassName}
+                  placeholder="Enter your email"
+                  type="email"
+                  autoComplete="email webauthn"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* fullname field */}
+        <FormField
+          control={form.control}
+          name="full_name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>full name</FormLabel>
+              <FormControl>
+                <Input
+                  className={classNames.inputClassName}
+                  placeholder="Enter your name"
+                  type="text"
+                  autoComplete="name webauthn"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* password field */}
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <InputPassword
+                  field={field}
+                  isShow={false}
+                  autoComplete="current-password webauthn"
+                  className={classNames.inputClassName}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="roles_id"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Role</FormLabel>
+              <FormControl>
+                <Select onValueChange={(value) => field.onChange([value])}>
+                  <SelectTrigger className="cursor-pointer !px-3">
+                    <SelectValue placeholder="Select user role" />
+                  </SelectTrigger>
+                  <SelectContent className="!px-1 !py-1">
+                    {isLoadingAllRoles && (
+                      <SelectItem
+                        disabled
+                        value="loading"
+                        className="cursor-not-allowed"
+                      >
+                        <Spinner className="size-4" />
+                      </SelectItem>
+                    )}
+                    {dataAllRoles?.map((role) => (
+                      <SelectItem
+                        key={role.id}
+                        value={role.id}
+                        className="cursor-pointer !px-3"
+                      >
+                        {role.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+export default FormCreateUser;
---
+++ src/features/admin/ModalCreateUser.tsx
@@ -0,0 +1,92 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { ReactNode } from 'react';
+import FormCreateUser from './FormCreateUser';
+import { useCreateUser } from './useCreateUser';
+import { toast } from 'sonner';
+import { createUserRequestT } from '@/schema/admin/createUserSchema';
+import { Spinner } from '@/components/ui/spinner';
+import { useQueryClient } from '@tanstack/react-query';
+
+interface IProps {
+  triggerButton: ReactNode;
+}
+
+const ModalCreateUser = ({ triggerButton }: IProps) => {
+  const { mutateCreateUser, isLoadingCreateUser } = useCreateUser();
+  const queryClient = useQueryClient();
+
+  const handleSubmit = (data: createUserRequestT) => {
+    console.log('create user data', data);
+
+    mutateCreateUser(data, {
+      onSuccess: (data) => {
+        toast.success(data.message);
+        queryClient.invalidateQueries({ queryKey: ['listAllUsers'] });
+        document.getElementById('close-create-user')?.click();
+      },
+      onError: (error) => {
+        toast.error(error.message || 'Failed to create user');
+      },
+    });
+  };
+
+  return (
+    <Dialog>
+      <div>
+        <DialogTrigger asChild>{triggerButton}</DialogTrigger>
+        <DialogContent className="!p-5 sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Create user</DialogTitle>
+            <DialogDescription>
+              You can create new users from here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <FormCreateUser
+            onSubmit={handleSubmit}
+            onError={(errors) => {
+              console.log('FormCreateUser errors', errors);
+              Object.values(errors).forEach((error) => {
+                if (error && 'message' in error) {
+                  toast.error(error.message as string);
+                }
+              });
+            }}
+            classNames={{ inputClassName: '!px-3' }}
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button
+                className="cursor-pointer !px-3"
+                id="close-create-user"
+                variant="outline"
+              >
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button
+              className="cursor-pointer !px-3"
+              disabled={isLoadingCreateUser}
+              form={'create-user-form'}
+              type="submit"
+            >
+              {isLoadingCreateUser ? <Spinner className="size-3" /> : 'Create'}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </div>
+    </Dialog>
+  );
+};
+
+export default ModalCreateUser;
