e480fca939ef007ea76ff800e5e15cdc8396d1fb
Mahmoud_Walid <modywmbadr@gmail.com>
2025-11-23 23:30:46 +0200

    feat: add admin layout component with sidebar and navbar
    The new AppLayoutAdmin component provides a consistent layout structure for admin pages, including a sidebar, navbar, and main content area with proper spacing and overflow handling. Uses SidebarProvider for state management and includes Toaster for notifications.

---
+++ src/components/AppLayoutAdmin.tsx
@@ -0,0 +1,26 @@
+import { Outlet } from 'react-router';
+import { Toaster } from 'sonner';
+import { Navbar } from './Navbar';
+import { SidebarInset, SidebarProvider } from './ui/sidebar';
+import AppSidebar from './AppSidebar';
+
+const AppLayoutAdmin = () => {
+  return (
+    <SidebarProvider>
+      {/* <div className={'flex h-[100dvh] flex-col overflow-x-hidden'}> */}
+      <AppSidebar />
+      <Navbar />
+      <SidebarInset className="flex flex-1 flex-col">
+        <main className="flex-1 overflow-auto">
+          <div className={'my-5 !ml-auto flex flex-col px-4 !py-3'}>
+            <Toaster className="select-none" />
+            <Outlet />
+          </div>
+        </main>
+      </SidebarInset>
+      {/* </div> */}
+    </SidebarProvider>
+  );
+};
+
+export default AppLayoutAdmin;
