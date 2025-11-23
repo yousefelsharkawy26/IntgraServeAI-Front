525a7efab67304b53f87c4c8bd117db90c330258
Mahmoud_Walid <modywmbadr@gmail.com>
2025-11-23 23:23:34 +0200

    feat(theme): add theme context and provider implementation
    Implement theme context with dark/light/system options
    Add ThemeContextProvider to handle theme state and persistence
    Include theme provider in AppProviders component

---
+++ src/providers/context/theme-context/ThemeContextProvider.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { Theme, ThemeContext, ThemeContextProps } from './ThemeContext';
+
+const ThemeContextProvider = ({
+  children,
+  defaultTheme = 'system',
+  storageKey = 'vite-ui-theme',
+  ...props
+}: ThemeContextProps) => {
+  const [theme, setTheme] = useState<Theme>(
+    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove('light', 'dark');
+
+    if (theme === 'system') {
+      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
+        .matches
+        ? 'dark'
+        : 'light';
+
+      root.classList.add(systemTheme);
+      return;
+    }
+
+    root.classList.add(theme);
+  }, [theme]);
+
+  const value = {
+    theme,
+    setTheme: (theme: Theme) => {
+      localStorage.setItem(storageKey, theme);
+      setTheme(theme);
+    },
+  };
+
+  return (
+    <ThemeContext.Provider {...props} value={value}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContextProvider;
